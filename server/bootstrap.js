becc11f79fe67a50c050a0803f610490d0a6ac29
Gary Nakanelua <gnakan@gmail.com>
2016-02-19 22:35:09 -0500

    updated

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,12 @@
 Meteor.startup(function(){
-
+  if(ValveMetrics.find().count() === 0){
+    ValveMetrics.insert({
+      _id: "bootstrap",
+      original_text: "Some test text",
+      permalink: "http://www.bpcs.com",
+      clean_text: "Some clean text", //need for sentiment analysis
+      submitted: moment().format('MMM Do YYYY, h:mm a'),
+      updated: moment().format('MMM Do YYYY, h:mm a')
+      });
+   }
 });
