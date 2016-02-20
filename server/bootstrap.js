b7fc1578d2b93e424505ce59e3b028a20291d908
Gary Nakanelua <gnakan@gmail.com>
2016-02-19 22:55:24 -0500

    Updated

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -9,17 +9,20 @@ Meteor.startup(function(){
       updated: moment().format('MMM Do YYYY, h:mm a')
       });
    }
+   else {
+     ValvePosts.remove("bootstrap");
+   }
 
 
   if(ValveMetrics.find().count() === 0){
     ValveMetrics.insert({
-      _id: "bootstrap",
+      _id: "main",
       ungraded: 0,
-      positive: 1,
+      positive: 0,
       neutral: 0,
       negative: 0,
-      total: 1,
-      score: 1,
+      total: 0,
+      score: 0,
       byMonth: [],
       updated: moment().format('MMM Do YYYY, h:mm a')
       });
